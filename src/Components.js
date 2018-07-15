19766d62d4b554eacc9688c3195b5b27abcad341
Charlie Guan <charliegdev@gmail.com>
2018-07-14 17:03:51 -0700

    Remove ng logic.

--- src/Components.js
+++ src/Components.js
@@ -4,39 +4,33 @@ const Menu = ({ isLoggedIn = true, changeLoginFunc }) => {
   return (
     <div className="ui inverted segment">
       <div className="ui inverted secondary fluid menu">
-        {isLoggedIn ? (
-          <a className="active item right orange" onClick={changeLoginFunc}>
-            Logout
-          </a>
-        ) : (
-          <a className="active item right blue" onClick={changeLoginFunc}>
-            Employee Login
-          </a>
-        )}
+        {isLoggedIn ? 
+          <a className="active item right orange" onClick={changeLoginFunc}>Logout</a>
+        : 
+          <a className="active item right blue" onClick={changeLoginFunc}>Employee Login</a>
+        }
       </div>
     </div>
   );
 };
 
 const BookItem = props => {
+  const { isLoggedIn } = props;
   const { title, author, genre, price, isbn } = props.bookInfo;
   return (
     <tr className="repeated-item">
-      <td>
-        <strong ng-hide="book.isUpdateMode">{title}</strong>
-      </td>
-      <td className="single line">
-        <span ng-hide="book.isUpdateMode">{author}</span>
-      </td>
-      <td>
-        <span ng-hide="book.isUpdateMode">{genre}</span>
-      </td>
-      <td>
-        <span ng-hide="book.isUpdateMode">{price}</span>
-      </td>
-      <td>
-        <span ng-hide="book.isUpdateMode">{isbn}</span>
-      </td>
+      <td>{title}</td>
+      <td className="single line">{author}</td>
+      <td>{genre}</td>
+      <td>{price}</td>
+      <td>{isbn}</td>
+      {isLoggedIn && 
+        <td>
+          <button className="yellow ui button">Update</button>
+          <button className="green ui button">Finish</button>
+        </td>
+      }
+      {isLoggedIn && <td><button className="negative ui button">Delete</button></td>}
     </tr>
   );
 };
@@ -53,11 +47,11 @@ const BookList = ({ isLoggedIn, data }) => {
             <th className="three wide">Genre</th>
             <th className="two wide">Price</th>
             <th className={isLoggedIn ? "two wide" : "four wide"}>ISBN</th>
+            {isLoggedIn && <th className="one wide">Update</th>}
+            {isLoggedIn && <th className="one wide">Delete</th>}
           </tr>
         </thead>
-        <tbody>
-          {data.map(book => <BookItem bookInfo={book} key={book.uuid} />)}
-        </tbody>
+        <tbody>{data.map(book => <BookItem bookInfo={book} key={book.uuid} isLoggedIn={isLoggedIn} />)}</tbody>
       </table>
     </div>
   );
